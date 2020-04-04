20c47b6394b8c1f6f6e9a612a88b2ab20e72beb9
kmkolasinski <kmkolasinski@gmail.com>
2020-04-04 10:48:24 +0200

    Add missing folder

---
+++ keras_detection/datasets/datasets_ops.py
@@ -0,0 +1,98 @@
+from typing import Dict, Optional, Callable, Tuple
+import tensorflow as tf
+from keras_detection.structures import ImageData, get_padding_shapes
+from keras_detection.ops.python_ops import NestedDict
+import keras_detection.ops.python_ops as py_ops
+
+ImageDataMapFn = Callable[[ImageData[tf.Tensor]], ImageData[ImageData[tf.Tensor]]]
+
+
+def prepare_dataset(
+    dataset: tf.data.Dataset,
+    model_image_size: Tuple[int, int],
+    augmentation_fn: Optional[ImageDataMapFn] = None,
+    num_epochs: Optional[int] = None,
+    batch_size: Optional[int] = None,
+    shuffle_buffer_size: Optional[int] = None,
+    num_parallel_calls: Optional[int] = None,
+    prefetch_buffer_size: Optional[int] = None,
+    prefetch_to_device: Optional[str] = None,
+) -> tf.data.Dataset:
+
+    # apply data augmentation:
+    if augmentation_fn is not None:
+        dataset = dataset.map(
+            map_image_data(augmentation_fn), num_parallel_calls=num_parallel_calls,
+        )
+
+    if shuffle_buffer_size is not None:
+        dataset = dataset.shuffle(buffer_size=shuffle_buffer_size)
+
+    dataset = dataset.repeat(num_epochs)
+    dataset = dataset.map(
+        map_image_data(prepare_for_batching(model_image_size)),
+        num_parallel_calls=num_parallel_calls,
+    )
+
+    # batching and padding
+    if batch_size is not None:
+        dataset = dataset.padded_batch(
+            batch_size=batch_size,
+            padded_shapes=get_padding_shapes(
+                dataset, spatial_image_shape=model_image_size
+            ),
+            drop_remainder=True,
+        )
+
+    # try to prefetch dataset on certain device
+    if prefetch_to_device is not None:
+        prefetch_fn = tf.data.experimental.prefetch_to_device(
+            device=prefetch_to_device, buffer_size=prefetch_buffer_size
+        )
+        dataset = dataset.apply(prefetch_fn)
+    else:
+        if prefetch_buffer_size is not None:
+            dataset = dataset.prefetch(buffer_size=prefetch_buffer_size)
+
+    return dataset
+
+
+def from_numpy_generator(generator) -> tf.data.Dataset:
+
+    example: NestedDict = next(generator)
+    output_types = py_ops.map_nested_dict(example, lambda e: tf.as_dtype(e.dtype))
+
+    # TODO fix output shapes to have defined, here only the last
+    #   dimension is not None
+    output_shapes = py_ops.map_nested_dict(
+        example,
+        lambda e: tf.TensorShape([None] * (len(e.shape) - 1) + [e.shape[-1]])
+        if len(e.shape) > 1
+        else [None],
+    )
+    return tf.data.Dataset.from_generator(
+        lambda: generator, output_types=output_types, output_shapes=output_shapes
+    )
+
+
+def map_image_data(
+    map_fn: ImageDataMapFn,
+) -> Callable[[Dict[str, tf.Tensor]], Dict[str, tf.Tensor]]:
+    def map_function(image_data: Dict[str, tf.Tensor]) -> Dict[str, tf.Tensor]:
+        data = ImageData.from_dict(image_data)
+        data = map_fn(data)
+        return data.to_dict()
+
+    return map_function
+
+
+def prepare_for_batching(size: Tuple[int, int]) -> ImageDataMapFn:
+    def map_fn(data: ImageData) -> ImageData:
+        image = tf.image.resize(data.features.image, size)
+        data = data.replace_image(image=image)
+        if data.has_labels():
+            num_rows = tf.shape(data.labels.boxes)[0]
+            data = data.replace_frame_field("num_rows", num_rows)
+        return data
+
+    return map_fn
---
+++ keras_detection/datasets/svhn/read_svhm_map.py
@@ -0,0 +1,204 @@
+import json
+import pickle
+from glob import glob
+from json import JSONEncoder
+from pathlib import Path
+from typing import Dict, Any, List, Optional, Tuple
+
+import h5py
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+import tensorflow as tf
+from keras_detection import Features, LabelsFrame, ImageData
+
+
+def load_dataset(dataset_dir: Path, as_numpy: bool = False):
+    dataset_dir = Path(dataset_dir)
+
+    train_annotations_file = dataset_dir / 'digitStruct.json'
+    digits_struct_file = dataset_dir / 'digitStruct.mat'
+    dataset_pkl_file = dataset_dir / (dataset_dir.name + ".pkl")
+
+    if dataset_pkl_file.exists():
+        dataset_np = pickle.load(dataset_pkl_file.open("rb"))
+        print(f"Loaded {dataset_pkl_file.name} dataset from existing pickle file. N={len(dataset_np)}")
+    else:
+        if not digits_struct_file.exists():
+            raise FileExistsError(f"Cannot open {digits_struct_file}")
+
+        if not train_annotations_file.exists():
+            print(f"Parsing {digits_struct_file}, this may take a while")
+            dsf = DigitStructFile(str(digits_struct_file))
+            annotations = dsf.load()
+            fout = open(train_annotations_file, 'w')
+            fout.write(JSONEncoder(indent=True).encode(annotations))
+            fout.close()
+        else:
+            print(f"Loading existing JSON annotations from {train_annotations_file}")
+            with open(train_annotations_file, 'r') as file:
+                annotations = json.load(file)
+
+        print(f"Loaded annotations for {len(annotations)} images")
+        print(f"Loading images ... ")
+        images = {
+            Path(p).name: np.array(Image.open(p))
+            for p in tqdm(glob(f"{dataset_dir}/*.png"))
+        }
+        dataset_np = prepare_dataset(images, annotations, dataset_pkl_file)
+
+    num_examples = len(dataset_np)
+    if as_numpy:
+        return dataset_np, num_examples
+
+    def dataset_generator(svhn_dataset):
+        for element in svhn_dataset:
+            yield element
+
+    dataset = tf.data.Dataset.from_generator(
+        lambda: dataset_generator(dataset_np),
+        ImageData.dataset_dtypes(), ImageData.dataset_shapes()
+    )
+    return dataset, num_examples
+
+
+def prepare_dataset(
+        images: Dict[str, np.ndarray],
+        annotations: List[Dict[str, Any]],
+        save_path: Optional[Path] = None
+) -> List[Any]:
+
+    assert isinstance(images, dict), "images must be a dict"
+    assert isinstance(annotations, list), "annotations must be a list"
+
+    if save_path is not None:
+        save_path = Path(save_path)
+
+    dataset = []
+    for features in tqdm(annotations):
+        image = images[features['filename']]
+        h, w = image.shape[:2]
+
+        boxes = []
+        labels = []
+        for b in features['boxes']:
+            top, left = b['top'], b['left']
+            height, width = b['height'], b['width']
+            box = [top, left, top + height, left + width]
+            labels.append(b['label'])
+            boxes.append(box)
+
+        labels = np.array(labels)
+        weights = np.ones_like(labels)
+        boxes = np.array(boxes) / np.array([h, w, h, w])
+
+        features = Features(image=image)
+        labels = LabelsFrame(
+            boxes=boxes,
+            labels=labels,
+            weights=weights
+        )
+        dataset.append(ImageData(features, labels).to_dict())
+
+    if save_path is not None:
+        print(f"Saving dataset to: {save_path}")
+        pickle.dump(dataset, save_path.open("wb"))
+
+    return dataset
+
+
+class DigitStructFile:
+    """
+    This class has been copied from internet ...
+
+
+    Example usage:
+
+    dsf = DigitStructFile(fin)
+    dataset = dsf.load()
+    fout = open(options.filePrefix + ".json",'w')
+    fout.write(JSONEncoder(indent = True).encode(dataset))
+    fout.close()
+
+
+    """
+    def __init__(self, inf):
+        """
+        The DigitStructFile is just a wrapper around the h5py data.  It basically references
+           inf:              The input h5 matlab file
+           digitStructName   The h5 ref to all the file names
+           digitStructBbox   The h5 ref to all struct data
+        """
+        self.inf = h5py.File(inf, "r")
+        self.digit_struct_name = self.inf["digitStruct"]["name"]
+        self.digit_struct_box = self.inf["digitStruct"]["bbox"]
+
+    def get_name(self, n: int) -> str:
+        """getName returns the 'name' string for for the n(th) digitStruct."""
+        return "".join([chr(c[0]) for c in self.inf[self.digit_struct_name[n][0]].value])
+
+    def bbox_helper(self, attr):
+        """bboxHelper handles the coding difference when there is exactly one bbox or an array of bbox."""
+        if len(attr) > 1:
+            attr = [
+                self.inf[attr.value[j].item()].value[0][0] for j in range(len(attr))
+            ]
+        else:
+            attr = [attr.value[0][0]]
+        return attr
+
+    def get_bbox(self, n: int) -> Dict[str, Any]:
+        """# getBbox returns a dict of data for the n(th) bbox."""
+        bbox = {}
+        bb = self.digit_struct_box[n].item()
+        bbox["height"] = self.bbox_helper(self.inf[bb]["height"])
+        bbox["label"] = self.bbox_helper(self.inf[bb]["label"])
+        bbox["left"] = self.bbox_helper(self.inf[bb]["left"])
+        bbox["top"] = self.bbox_helper(self.inf[bb]["top"])
+        bbox["width"] = self.bbox_helper(self.inf[bb]["width"])
+        return bbox
+
+    def get_digit_structure(self, n):
+        s = self.get_bbox(n)
+        s["name"] = self.get_name(n)
+        return s
+
+    def get_all_digit_structure(self):
+        """
+        # getAllDigitStructure returns all the digitStruct from the input file.
+
+        """
+        return [self.get_digit_structure(i) for i in range(len(self.digit_struct_name))]
+
+    def load(self):
+        """
+        Return a restructured version of the dataset (one structure by boxed digit).
+
+          Return a list of such dicts :
+             'filename' : filename of the samples
+             'boxes' : list of such dicts (one by digit) :
+                 'label' : 1 to 9 corresponding digits. 10 for digit '0' in image.
+                 'left', 'top' : position of bounding box
+                 'width', 'height' : dimension of bounding box
+
+        Note: We may turn this to a generator, if memory issues arise.
+        """
+        pictDat = self.get_all_digit_structure()
+        result = []
+        structCnt = 1
+        for i in range(len(pictDat)):
+            item = {"filename": pictDat[i]["name"]}
+            figures = []
+            for j in range(len(pictDat[i]["height"])):
+                figure = {
+                    "height": pictDat[i]["height"][j],
+                    "label": pictDat[i]["label"][j],
+                    "left": pictDat[i]["left"][j],
+                    "top": pictDat[i]["top"][j],
+                    "width": pictDat[i]["width"][j],
+                }
+                figures.append(figure)
+            structCnt = structCnt + 1
+            item["boxes"] = figures
+            result.append(item)
+        return result
