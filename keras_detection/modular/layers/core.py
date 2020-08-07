622198624db86a0b9ba3653df71859ed2921ae79
kkol <krzysztof.kolasinski@shelfwise.ai>
2020-08-07 22:46:03 +0200

    Quick change

--- keras_detection/modular/layers/core.py
+++ keras_detection/modular/layers/core.py
@@ -39,7 +39,7 @@ class ROISamplingLayer(TrainableModule):
         }
 
 
-class ROINMSSamplingLayer(TrainableModule):
+class ROINMSSamplingLayer(Module):
     def __init__(self, num_samples: int, crop_size: Tuple[int, int], **kwargs):
         super().__init__(**kwargs)
         self.crop_size = crop_size
